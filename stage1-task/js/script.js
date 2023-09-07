00b02bbb0da1f7b94ba1f57170eeddf6b0879436
Daniel Ezekiel <dbezekiel77@gmail.com>
2023-09-07 14:06:32 +0100

    feat: corrected UTC time in milliseconds setup

--- stage1-task/js/script.js
+++ stage1-task/js/script.js
@@ -19,24 +19,9 @@ const getTodayDate = (_) => {
 const getDay = (_) => {
   return getTodayDate().getDay();
 };
-const getUTCValues = () => {
-  const todayDate = getTodayDate();
-  const UTCValues = [];
-
-  const year = todayDate.getUTCFullYear();
-  const monthIndex = todayDate.getUTCMonth();
-  const day = todayDate.getUTCDay();
-  const hours = todayDate.getUTCHours();
-  const minutes = todayDate.getUTCMinutes();
-  const seconds = todayDate.getUTCSeconds();
-  const milliseconds = todayDate.getUTCMilliseconds();
-
-  UTCValues.push(year, monthIndex, day, hours, minutes, seconds, milliseconds);
-  return UTCValues;
-};
 
 labelDay.textContent = weekDays[getDay()];
 // Show UTC Time in Milliseconds using the setInterval API
 setInterval(() => {
-  labelTime.textContent = Date.UTC(...getUTCValues());
+  labelTime.textContent = Date.now();
 }, 1000);
