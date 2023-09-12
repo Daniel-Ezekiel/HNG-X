76d138e764a87c210f6d64e0fabdd2866cb7fe71
Daniel Ezekiel <dbezekiel77@gmail.com>
2023-09-12 11:47:42 +0100

    feat: render movie cards with 10 movies

--- stage2-task/src/pages/Homepage.jsx
+++ stage2-task/src/pages/Homepage.jsx
@@ -1,6 +1,6 @@
-import Footer from "../components/Footer";
 import TopSection from "../components/TopSection";
 import FeaturedMovies from "../components/FeaturedMovies";
+import Footer from "../components/Footer";
 
 const Homepage = () => {
   return (
