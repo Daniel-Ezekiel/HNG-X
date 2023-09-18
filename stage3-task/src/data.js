f5b8608382965d74f7a4858ade0194363807d157
Daniel Ezekiel <dbezekiel77@gmail.com>
2023-09-19 00:06:18 +0100

    fix: images not being visible

--- stage3-task/src/data.js
+++ stage3-task/src/data.js
@@ -3,70 +3,70 @@
 export default [
   {
     id: 1,
-    src: "./src/assets/images/1.jpg",
+    src: "./public/images/1.jpg",
     tags: ["football", "wallpaper"],
     type: "landscape",
   },
 
   {
     id: 2,
-    src: "./src/assets/images/2.jpg",
+    src: "./public/images/2.jpg",
     tags: ["football", "wallpaper"],
     type: "landscape",
   },
 
   {
     id: 3,
-    src: "./src/assets/images/3.jpg",
+    src: "./public/images/3.jpg",
     tags: ["football", "wallpaper"],
     type: "landscape",
   },
 
   {
     id: 4,
-    src: "./src/assets/images/4.jpg",
+    src: "./public/images/4.jpg",
     tags: ["football", "wallpaper"],
     type: "landscape",
   },
 
   {
     id: 5,
-    src: "./src/assets/images/5.jpg",
+    src: "./public/images/5.jpg",
     tags: ["football", "wallpaper"],
     type: "landscape",
   },
 
   {
     id: 6,
-    src: "./src/assets/images/6.jpg",
+    src: "./public/images/6.jpg",
     tags: ["football", "wallpaper"],
     type: "landscape",
   },
 
   {
     id: 7,
-    src: "./src/assets/images/7.jpg",
+    src: "./public/images/7.jpg",
     tags: ["football", "wallpaper"],
     type: "landscape",
   },
 
   {
     id: 8,
-    src: "./src/assets/images/8.jpg",
+    src: "./public/images/8.jpg",
     tags: ["football", "wallpaper"],
     type: "landscape",
   },
 
   {
     id: 9,
-    src: "./src/assets/images/9.jpg",
+    src: "./public/images/9.jpg",
     tags: ["football", "wallpaper"],
     type: "landscape",
   },
 
   {
     id: 10,
-    src: "./src/assets/images/10.jpg",
+    src: "./public/images/10.jpg",
     tags: ["football", "wallpaper"],
     type: "landscape",
   },
