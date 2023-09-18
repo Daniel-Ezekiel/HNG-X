07bd976d3147714e52bb694916c170f15338f540
Daniel Ezekiel <dbezekiel77@gmail.com>
2023-09-18 20:02:43 +0100

    feat: UI setup for gallery app

--- stage3-task/src/App.jsx
+++ stage3-task/src/App.jsx
@@ -1,35 +1,47 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { SearchOutlined } from "@mui/icons-material";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <header className='flex justify-between p-4 leading-7'>
+        <h1 className='font-bold w-[10rem] text-xl italic '>
+          Galleria D'Imaggini
+        </h1>
+        <button className=' hover:underline'>Sign In</button>
+      </header>
+      <main className='grid gap-4 p-4'>
+        <div className='relative'>
+          <input
+            type='text'
+            placeholder='Search images using tags'
+            className='w-full p-1 pr-5 border rounded-md'
+          />
+          <button className='absolute top-1/2 right-1 translate-y-[-50%]'>
+            <SearchOutlined />
+          </button>
+        </div>
+
+        <div className='mt-5'>
+          <div className='relative rounded-xl overflow-hidden shadow-2xl cursor-grab'>
+            <img
+              src='./src/assets/images/10.jpg'
+              alt=''
+              className='object-cover scale-110 hover:scale-100 transition-all duration-500 ease-in-out'
+            />
+            <div className='absolute flex gap-2 left-2 bottom-2'>
+              <span className='px-2 py-1 bg-[rgba(255,255,255,0.7)] rounded-full text-sm'>
+                football
+              </span>
+              <span className='px-2 py-1 bg-[rgba(255,255,255,0.7)] rounded-full text-sm'>
+                wallpaper
+              </span>
+            </div>
+          </div>
+        </div>
+      </main>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
+
--- stage3-task/tailwind.config.js
+++ stage3-task/tailwind.config.js
@@ -1,9 +1,57 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    fontSize: {
+      sm: "1.2rem",
+      base: "1.5rem",
+      xl: "1.8rem",
+      "2xl": "2.4rem",
+      "3xl": "3.2rem",
+    },
+
+    spacing: {
+      1: "0.5rem",
+      2: "1rem",
+      3: "1.5rem",
+      4: "2rem",
+      5: "2.5rem",
+      6: "3rem",
+      7: "3.5rem",
+      8: "4rem",
+      9: "4.5rem",
+      10: "5rem",
+    },
+
+    borderWidth: {
+      DEFAULT: "0.1rem",
+      0: "0",
+      2: "0.2rem",
+      3: "0.3rem",
+      4: "0.4rem",
+      5: "0.5rem",
+    },
+
+    fontFamily: {
+      sans: ["Manrope", "sans-serif"],
+    },
+
+    colors: {
+      rose: "#be123c",
+      gray: "#9ca3af",
+      "dark-gray": "#111827",
+      white: "#fff",
+    },
+
+    screens: {
+      sm: "540px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+      "2xl": "1440px",
+      "3xl": "1920px",
+    },
   },
   plugins: [],
-}
+};
 
