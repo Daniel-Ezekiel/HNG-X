4865bbbc99b4a80a1a294699af5c82400f15e981
Daniel Ezekiel <dbezekiel77@gmail.com>
2023-09-21 10:53:51 +0100

    feat: dnd functionality fully set up and image layout updated

--- stage3-task/src/components/MainContent.jsx
+++ stage3-task/src/components/MainContent.jsx
@@ -1,47 +1,14 @@
 import { Close, SearchOutlined } from "@mui/icons-material";
 import ImageBox from "./ImageBox";
 import imgData from "../data";
-import { useState } from "react";
-import { DndContext, closestCenter } from "@dnd-kit/core";
-import {
-  SortableContext,
-  arrayMove,
-  rectSortingStrategy,
-  useSortable,
-} from "@dnd-kit/sortable";
-import { CSS } from "@dnd-kit/utilities";
-import Tag from "./Tag";
-
-const SortableImage = ({ image }) => {
-  const { attributes, listeners, setNodeRef, transform, transition } =
-    useSortable({ id: image.id });
-  const style = { transition, transform: CSS.Transform.toString(transform) };
-  const tagSpans = image.tags.map((tag, i) => <Tag key={i} tagVal={tag} />);
-
-  return (
-    <div
-      ref={setNodeRef}
-      style={style}
-      {...attributes}
-      {...listeners}
-      className='relative rounded-xl overflow-hidden shadow-2xl cursor-grab'
-    >
-      <img
-        src={image.src}
-        alt={`Gallery Image ${image.id}`}
-        className='w-full h-full object-cover scale-110 hover:scale-100 transition-all duration-500 ease-in-out'
-      />
-      <div className='absolute flex flex-wrap gap-1 left-2 bottom-2'>
-        {tagSpans}
-      </div>
-    </div>
-  );
-};
+import { useEffect, useRef, useState } from "react";
 
 const MainContent = ({ userLoggedIn }) => {
   const [allImages, setAllImages] = useState(imgData);
   const [searchVal, setSearchVal] = useState("");
   const [searchTags, setSearchTags] = useState([]);
+  const dragImage = useRef(0);
+  const draggedOverImage = useRef(0);
 
   const handleSearch = () => {
     const uniqueTags = [...new Set(searchTags), searchVal.trim().toLowerCase()];
@@ -56,27 +23,29 @@ const MainContent = ({ userLoggedIn }) => {
     setSearchTags(newSearchTags);
   };
 
-  const imgElements = searchTags.length
-    ? allImages
-        .filter((data) => data.tags.some((tag) => searchTags.includes(tag)))
-        .map((data, i) => (
-          <SortableImage
-            key={i}
-            image={data}
-            // id={data.id}
-            // src={data.src}
-            // tags={data.tags}
-          />
-        ))
-    : allImages.map((data, i) => (
-        <SortableImage
-          key={i}
-          image={data}
-          // id={data.id}
-          // src={data.src}
-          // tags={data.tags}
-        />
-      ));
+  const handleSort = () => {
+    const allImagesClone = [...allImages];
+    const temp = allImagesClone[dragImage.current];
+    allImagesClone[dragImage.current] =
+      allImagesClone[draggedOverImage.current];
+    allImagesClone[draggedOverImage.current] = temp;
+
+    setAllImages(allImagesClone);
+  };
+
+  const imgElements = allImages.map((data, i) => (
+    <ImageBox
+      key={i}
+      id={data.id}
+      src={data.src}
+      tags={data.tags}
+      orientation={data.type}
+      handleDragStart={() => (dragImage.current = i)}
+      handleDragEnter={() => (draggedOverImage.current = i)}
+      handleDragEnd={handleSort}
+      handleDragOver={(event) => event.preventDefault()}
+    />
+  ));
 
   const tagElements = searchTags.map((tag, i) =>
     tag ? (
@@ -95,20 +64,17 @@ const MainContent = ({ userLoggedIn }) => {
       </span>
     ) : null
   );
-  // console.log(tagElements);
 
-  const onDragEnd = (event) => {
-    // console.log("onDragEnd", event);
-    const { active, over } = event;
-    if (active.id === over.id) return;
-
-    setAllImages((allImages) => {
-      const oldIndex = allImages.findIndex((image) => (image.id = active.id));
-      const newIndex = allImages.findIndex((image) => (image.id = over.id));
-
-      return arrayMove(allImages, oldIndex, newIndex);
-    });
-  };
+  useEffect(() => {
+    if (searchTags.length) {
+      const newImgArr = imgData.filter((data) =>
+        data.tags.some((tag) => searchTags.includes(tag))
+      );
+      setAllImages(newImgArr);
+    } else {
+      setAllImages(imgData);
+    }
+  }, [searchTags]);
 
   return (
     <main className='grid gap-4 p-4 max-w-[120rem] mx-auto'>
@@ -130,19 +96,15 @@ const MainContent = ({ userLoggedIn }) => {
 
       <div className='flex flex-wrap gap-3'>{tagElements}</div>
 
-      <DndContext collisionDetection={closestCenter} onDragEnd={onDragEnd}>
-        <SortableContext items={allImages} strategy={rectSortingStrategy}>
-          <div className='mt-5 grid gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4'>
-            {imgElements.length ? (
-              imgElements
-            ) : (
-              <p className='text-base text-center'>
-                Sorry, your search did not give any results.
-              </p>
-            )}
-          </div>
-        </SortableContext>
-      </DndContext>
+      <div className='mt-5 grid gap-4 grid-cols-2 grid-flow-dense md:grid-cols-3 lg:grid-cols-4'>
+        {imgElements.length ? (
+          imgElements
+        ) : (
+          <p className='text-base text-center'>
+            Sorry, your search did not give any results.
+          </p>
+        )}
+      </div>
     </main>
   );
 };
--- stage3-task/src/data.js
+++ stage3-task/src/data.js
@@ -12,14 +12,14 @@ export default [
     id: 2,
     src: "images/2.jpg",
     tags: ["cartoon", "cats", "wallpaper"],
-    type: "landscape",
+    type: "portrait",
   },
 
   {
     id: 3,
     src: "images/3.jpg",
     tags: ["anime", "naruto", "wallpaper"],
-    type: "landscape",
+    type: "portrait",
   },
 
   {
@@ -33,41 +33,41 @@ export default [
     id: 5,
     src: "images/5.jpg",
     tags: ["anime", "wallpaper"],
-    type: "landscape",
+    type: "portrait",
   },
 
   {
     id: 6,
     src: "images/6.jpg",
     tags: ["game", "ghost", "cod", "wallpaper"],
-    type: "landscape",
+    type: "portrait",
   },
 
   {
     id: 7,
     src: "images/7.jpg",
     tags: ["cartoon", "luca", "wallpaper"],
-    type: "landscape",
+    type: "portrait",
   },
 
   {
     id: 8,
     src: "images/8.jpg",
     tags: ["person", "wallpaper"],
-    type: "landscape",
+    type: "portrait",
   },
 
   {
     id: 9,
-    src: "images/9.jpg",
-    tags: ["robot", "wallpaper"],
+    src: "images/10.jpg",
+    tags: ["messi", "goat", "football", "wallpaper"],
     type: "landscape",
   },
 
   {
     id: 10,
-    src: "images/10.jpg",
-    tags: ["messi", "goat", "football", "wallpaper"],
-    type: "landscape",
+    src: "images/9.jpg",
+    tags: ["robot", "wallpaper"],
+    type: "portrait",
   },
 ];
