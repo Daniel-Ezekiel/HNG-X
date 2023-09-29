642677afcbae3d931924e036367b50ea5b538b81
Daniel Ezekiel <dbezekiel77@gmail.com>
2023-09-29 19:18:20 +0100

    feat: login page ui setup

--- stage5-task/src/App.jsx
+++ stage5-task/src/App.jsx
@@ -1,11 +1,17 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./App.css";
 import Home from "./pages/Home";
+import Login from "./pages/Login";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Home />,
+    // errorElement: <ErrorPage />,
+  },
+  {
+    path: "/login",
+    element: <Login />,
     // errorElement: <ErrorPage />,
   },
 ]);
--- stage5-task/src/components/Header.jsx
+++ stage5-task/src/components/Header.jsx
@@ -1,14 +1,17 @@
 import { Menu } from "@mui/icons-material";
 import logo from "../assets/logo.png";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
     <header className='border-b-[0.2rem] border-b-[#f4f6f8] p-4 text-sm'>
       <nav className='grid grid-cols-3 items-center md:flex md:justify-between md:max-w-[124rem] md:mx-auto'>
-        <span className='col-start-2 flex gap-1 items-center font-inter font-bold text-blue'>
-          <img src={logo} alt='logo' />
-          HelpMeOut
-        </span>
+        <Link to='/'>
+          <span className='col-start-2 flex gap-1 items-center font-inter font-bold text-blue'>
+            <img src={logo} alt='logo' />
+            HelpMeOut
+          </span>
+        </Link>
 
         <ul className='hidden font-work-sans font-medium text-sm md:flex md:gap-4'>
           <li>
